044f8a7702eba82436b2a24078e11070432228c2
Çağıl Alsaç <cagilalsac@outlook.com>
2022-06-04 04:39:40 +0300

    Add project files.

---
+++ ScaffoldApp/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ScaffoldApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbUserPath = new System.Windows.Forms.TextBox();
+            this.bUpdate = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.bBrowse = new System.Windows.Forms.Button();
+            this.bReset = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbControllerPath = new System.Windows.Forms.TextBox();
+            this.bControllerOpen = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbViewPath = new System.Windows.Forms.TextBox();
+            this.bViewOpen = new System.Windows.Forms.Button();
+            this.bBackup = new System.Windows.Forms.Button();
+            this.bDbContext = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Windows User Folder:";
+            // 
+            // tbUserPath
+            // 
+            this.tbUserPath.Location = new System.Drawing.Point(139, 12);
+            this.tbUserPath.Name = "tbUserPath";
+            this.tbUserPath.ReadOnly = true;
+            this.tbUserPath.Size = new System.Drawing.Size(671, 23);
+            this.tbUserPath.TabIndex = 1;
+            // 
+            // bUpdate
+            // 
+            this.bUpdate.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bUpdate.ForeColor = System.Drawing.Color.Red;
+            this.bUpdate.Location = new System.Drawing.Point(139, 41);
+            this.bUpdate.Name = "bUpdate";
+            this.bUpdate.Size = new System.Drawing.Size(276, 23);
+            this.bUpdate.TabIndex = 2;
+            this.bUpdate.Text = "Update Modified Files Without DbContext";
+            this.bUpdate.UseVisualStyleBackColor = true;
+            this.bUpdate.Click += new System.EventHandler(this.bUpdate_Click);
+            // 
+            // bBrowse
+            // 
+            this.bBrowse.Location = new System.Drawing.Point(816, 11);
+            this.bBrowse.Name = "bBrowse";
+            this.bBrowse.Size = new System.Drawing.Size(75, 23);
+            this.bBrowse.TabIndex = 3;
+            this.bBrowse.Text = "Browse";
+            this.bBrowse.UseVisualStyleBackColor = true;
+            this.bBrowse.Click += new System.EventHandler(this.bBrowse_Click);
+            // 
+            // bReset
+            // 
+            this.bReset.Location = new System.Drawing.Point(897, 11);
+            this.bReset.Name = "bReset";
+            this.bReset.Size = new System.Drawing.Size(75, 23);
+            this.bReset.TabIndex = 4;
+            this.bReset.Text = "Reset";
+            this.bReset.UseVisualStyleBackColor = true;
+            this.bReset.Click += new System.EventHandler(this.bReset_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Scaffolding Controller Folder:";
+            // 
+            // tbControllerPath
+            // 
+            this.tbControllerPath.Location = new System.Drawing.Point(180, 119);
+            this.tbControllerPath.Name = "tbControllerPath";
+            this.tbControllerPath.ReadOnly = true;
+            this.tbControllerPath.Size = new System.Drawing.Size(711, 23);
+            this.tbControllerPath.TabIndex = 6;
+            // 
+            // bControllerOpen
+            // 
+            this.bControllerOpen.Location = new System.Drawing.Point(897, 119);
+            this.bControllerOpen.Name = "bControllerOpen";
+            this.bControllerOpen.Size = new System.Drawing.Size(75, 23);
+            this.bControllerOpen.TabIndex = 7;
+            this.bControllerOpen.Text = "Open";
+            this.bControllerOpen.UseVisualStyleBackColor = true;
+            this.bControllerOpen.Click += new System.EventHandler(this.bControllerOpen_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Scaffolding View Folder:";
+            // 
+            // tbViewPath
+            // 
+            this.tbViewPath.Location = new System.Drawing.Point(180, 148);
+            this.tbViewPath.Name = "tbViewPath";
+            this.tbViewPath.ReadOnly = true;
+            this.tbViewPath.Size = new System.Drawing.Size(711, 23);
+            this.tbViewPath.TabIndex = 9;
+            // 
+            // bViewOpen
+            // 
+            this.bViewOpen.Location = new System.Drawing.Point(897, 148);
+            this.bViewOpen.Name = "bViewOpen";
+            this.bViewOpen.Size = new System.Drawing.Size(75, 23);
+            this.bViewOpen.TabIndex = 10;
+            this.bViewOpen.Text = "Open";
+            this.bViewOpen.UseVisualStyleBackColor = true;
+            this.bViewOpen.Click += new System.EventHandler(this.bViewOpen_Click);
+            // 
+            // bBackup
+            // 
+            this.bBackup.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bBackup.ForeColor = System.Drawing.Color.Red;
+            this.bBackup.Location = new System.Drawing.Point(677, 41);
+            this.bBackup.Name = "bBackup";
+            this.bBackup.Size = new System.Drawing.Size(295, 23);
+            this.bBackup.TabIndex = 11;
+            this.bBackup.Text = "Restore Microsoft Backup Files";
+            this.bBackup.UseVisualStyleBackColor = true;
+            this.bBackup.Click += new System.EventHandler(this.bBackup_Click);
+            // 
+            // bDbContext
+            // 
+            this.bDbContext.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bDbContext.ForeColor = System.Drawing.Color.Red;
+            this.bDbContext.Location = new System.Drawing.Point(421, 41);
+            this.bDbContext.Name = "bDbContext";
+            this.bDbContext.Size = new System.Drawing.Size(250, 23);
+            this.bDbContext.TabIndex = 12;
+            this.bDbContext.Text = "Update Modified Files With DbContext";
+            this.bDbContext.UseVisualStyleBackColor = true;
+            this.bDbContext.Click += new System.EventHandler(this.bDbContext_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(947, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "If you have any problems about scaffolding with Entity Framework in your MVC Appl" +
+    "ication, first update all your Nuget Packages in all projects in the solution to" +
+    " the latest version.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 256);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(409, 15);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "If the problem is not solved, update Visual Studio with Visual Studio Installer.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 281);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.bDbContext);
+            this.Controls.Add(this.bBackup);
+            this.Controls.Add(this.bViewOpen);
+            this.Controls.Add(this.tbViewPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.bControllerOpen);
+            this.Controls.Add(this.tbControllerPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bReset);
+            this.Controls.Add(this.bBrowse);
+            this.Controls.Add(this.bUpdate);
+            this.Controls.Add(this.tbUserPath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Scaffold App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox tbUserPath;
+        private Button bUpdate;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Button bBrowse;
+        private Button bReset;
+        private Label label2;
+        private TextBox tbControllerPath;
+        private Button bControllerOpen;
+        private Label label3;
+        private TextBox tbViewPath;
+        private Button bViewOpen;
+        private Button bBackup;
+        private Button bDbContext;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ ScaffoldApp/Form1.cs
@@ -0,0 +1,309 @@
+using System.Diagnostics;
+
+namespace ScaffoldApp
+{
+    public partial class Form1 : Form
+    {
+        string startupPath;
+        string userPath;
+        string nuget;
+        string backup;
+        string dbContext;
+        string[] userDirectories;
+        string nugetPath;
+        string controllerGeneratorPath;
+        string controllerGeneratorUpdatePath;
+        string viewGeneratorPath;
+        string viewGeneratorUpdatePath;
+        string lastVersionedDirectory;
+        string sourceFileName;
+        string destinationFileName;
+        string[] files;
+        List<string> versionedDirectories;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                startupPath = Application.StartupPath.Replace(@"bin\Debug\net6.0-windows", "").Replace(@"bin\Release\net6.0-windows", "");
+                userPath = @"C:\Users\" + Environment.UserName;
+                nuget = ".nuget";
+                backup = "_Backup";
+                dbContext = "_DbContext";
+                nugetPath = nuget + @"\packages\microsoft.visualstudio.web.codegenerators.mvc";
+                controllerGeneratorPath = @"Templates\ControllerGenerator";
+                viewGeneratorPath = @"Templates\ViewGenerator";
+                tbUserPath.Text = userPath;
+                UpdateControllerAndViewPaths();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("An error occured!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        void UpdateControllerAndViewPaths()
+        {
+            userDirectories = Directory.GetDirectories(tbUserPath.Text);
+            if (!userDirectories.Any(ud => ud.EndsWith(nuget)))
+            {
+                MessageBox.Show(nuget + " folder could not be found!\n\nPlease use Entity Framework controller scaffolding once in your project for downloading necessary Nuget packages!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+                tbUserPath.Text = userPath;
+                return;
+            }
+            versionedDirectories = Directory.GetDirectories(tbUserPath.Text + @"\" + nugetPath).OrderBy(d => d).ToList();
+            lastVersionedDirectory = versionedDirectories.LastOrDefault();
+            if (lastVersionedDirectory == null)
+            {
+                MessageBox.Show("Version directory could not be found!\n\nPlease use Entity Framework controller scaffolding once in your project for downloading necessary Nuget packages!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+                tbUserPath.Text = userPath;
+                return;
+            }
+            controllerGeneratorUpdatePath = lastVersionedDirectory + @"\" + controllerGeneratorPath;
+            viewGeneratorUpdatePath = lastVersionedDirectory + @"\" + viewGeneratorPath;
+            tbControllerPath.Text = controllerGeneratorUpdatePath;
+            tbViewPath.Text = viewGeneratorUpdatePath;
+        }
+
+        private void bUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UpdateControllerAndViewPaths();
+                files = Directory.GetFiles(startupPath + @"\Generators\Controller").Where(f => !(f.Contains(backup) || f.Contains(dbContext))).ToArray();
+                bool found = true;
+                foreach (var file in files)
+                {
+                    sourceFileName = Path.GetFileName(file);
+                    destinationFileName = Path.Combine(controllerGeneratorUpdatePath, sourceFileName);
+                    if (!File.Exists(destinationFileName))
+                    {
+                        found = false;
+                        break;
+                    }
+                    File.Copy(file, destinationFileName, true);
+                }
+                if (!found)
+                {
+                    MessageBox.Show("Files copy canceled! Files not found!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+                }
+                else
+                {
+                    files = Directory.GetFiles(startupPath + @"\Generators\View").Where(f => !f.Contains(backup)).ToArray();
+                    found = true;
+                    foreach (var file in files)
+                    {
+                        sourceFileName = Path.GetFileName(file);
+                        destinationFileName = Path.Combine(viewGeneratorUpdatePath, sourceFileName);
+                        if (!File.Exists(destinationFileName))
+                        {
+                            found = false;
+                            break;
+                        }
+                        File.Copy(file, destinationFileName, true);
+                    }
+                    if (!found)
+                    {
+                        MessageBox.Show("Files copy canceled! Files not found!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Files copied successfully.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("An error occured during copying files!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void bBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    tbUserPath.Text = folderBrowserDialog1.SelectedPath;
+                    UpdateControllerAndViewPaths();
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("An error occured!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void bReset_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tbUserPath.Text = userPath;
+                UpdateControllerAndViewPaths();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("An error occured!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void bControllerOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Directory.Exists(tbControllerPath.Text))
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        Arguments = tbControllerPath.Text,
+                        FileName = "explorer.exe"
+                    };
+                    Process.Start(startInfo);
+                }
+                else
+                {
+                    MessageBox.Show("Directory could not be found!");
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("An error occured!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void bViewOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Directory.Exists(tbViewPath.Text))
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        Arguments = tbViewPath.Text,
+                        FileName = "explorer.exe"
+                    };
+                    Process.Start(startInfo);
+                }
+                else
+                {
+                    MessageBox.Show("Directory could not be found!");
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("An error occured!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void bBackup_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UpdateControllerAndViewPaths();
+                files = Directory.GetFiles(startupPath + @"\Generators\Controller").Where(f => f.Contains(backup)).ToArray();
+                bool found = true;
+                foreach (var file in files)
+                {
+                    sourceFileName = Path.GetFileName(file);
+                    destinationFileName = Path.Combine(controllerGeneratorUpdatePath, sourceFileName.Replace(backup, ""));
+                    if (!File.Exists(destinationFileName))
+                    {
+                        found = false;
+                        break;
+                    }
+                    File.Copy(file, destinationFileName, true);
+                }
+                if (!found)
+                {
+                    MessageBox.Show("Files copy canceled! Files not found!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+                }
+                else
+                {
+                    files = Directory.GetFiles(startupPath + @"\Generators\View").Where(f => f.Contains(backup)).ToArray();
+                    found = true;
+                    foreach (var file in files)
+                    {
+                        sourceFileName = Path.GetFileName(file);
+                        destinationFileName = Path.Combine(viewGeneratorUpdatePath, sourceFileName.Replace(backup, ""));
+                        if (!File.Exists(destinationFileName))
+                        {
+                            found = false;
+                            break;
+                        }
+                        File.Copy(file, destinationFileName, true);
+                    }
+                    if (!found)
+                    {
+                        MessageBox.Show("Files copy canceled! Files not found!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Files copied successfully.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("An error occured during copying files!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void bDbContext_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UpdateControllerAndViewPaths();
+                files = Directory.GetFiles(startupPath + @"\Generators\Controller").Where(f => f.Contains(dbContext)).ToArray();
+                bool found = true;
+                foreach (var file in files)
+                {
+                    sourceFileName = Path.GetFileName(file);
+                    destinationFileName = Path.Combine(controllerGeneratorUpdatePath, sourceFileName.Replace(dbContext, ""));
+                    if (!File.Exists(destinationFileName))
+                    {
+                        found = false;
+                        break;
+                    }
+                    File.Copy(file, destinationFileName, true);
+                }
+                if (!found)
+                {
+                    MessageBox.Show("Files copy canceled! Files not found!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+                }
+                else
+                {
+                    files = Directory.GetFiles(startupPath + @"\Generators\View").Where(f => !f.Contains(backup)).ToArray();
+                    found = true;
+                    foreach (var file in files)
+                    {
+                        sourceFileName = Path.GetFileName(file);
+                        destinationFileName = Path.Combine(viewGeneratorUpdatePath, sourceFileName.Replace(backup, ""));
+                        if (!File.Exists(destinationFileName))
+                        {
+                            found = false;
+                            break;
+                        }
+                        File.Copy(file, destinationFileName, true);
+                    }
+                    if (!found)
+                    {
+                        MessageBox.Show("Files copy canceled! Files not found!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Files copied successfully.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("An error occured during copying files!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+    }
+}
